456bf81f51da7fda1c4a32c8f6b2e63de8cd9273
Raphael Renato <rapha_rmm@hotmail.com>
2022-11-17 19:51:04 -0300

    ✨ Create participant component and add FlatList

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,11 +1,25 @@
 import React from "react";
-import { Alert, Text, TextInput, TouchableOpacity, View } from "react-native";
+import {
+  Alert,
+  FlatList,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from "react-native";
+
+import { Participant } from "../../components/Participant";
 
 import { styles } from "./styles";
 
 export function Home() {
+  const participants = [];
   function handleParticipantAdd() {
     Alert.alert("Participante", "Participante foi adicionado!");
+  }
+
+  function handleParticipantRemove() {
+    Alert.alert("Participante", "Participante foi removido!");
   }
 
   return (
@@ -24,6 +38,25 @@ export function Home() {
           <Text style={styles.buttonText}>+</Text>
         </TouchableOpacity>
       </View>
+
+      <FlatList
+        data={participants}
+        keyExtractor={(item) => item}
+        renderItem={({ item }) => (
+          <Participant
+            key={item}
+            name={item}
+            onRemove={() => handleParticipantRemove()}
+          />
+        )}
+        showsVerticalScrollIndicator={false}
+        ListEmptyComponent={() => (
+          <Text style={styles.listEmptyText}>
+            Ninguém chegou ao evento ainda? Adicione participantes a sua lista
+            de presença.
+          </Text>
+        )}
+      />
     </View>
   );
 }
